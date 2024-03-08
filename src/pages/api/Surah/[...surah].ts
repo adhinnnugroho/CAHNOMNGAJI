41171e4beb196ef32040d4086b3dfbdb83f1ea44
adhinnnugroho <adhinnnugroho@gmail.com>
2024-03-08 21:30:05 +0700

    feat: setup endpoint api to env

--- src/pages/api/Surah/[...surah].ts
+++ src/pages/api/Surah/[...surah].ts
@@ -7,13 +7,31 @@ export default async function handler(
 ) {
     if (req.method === "GET") {
         try {
-            const Surah = await instance.get('https://equran.id/api/v2/surat');
+
+            const Surah = await instance.get(`${process.env.API_URL}`);
             res.status(200).json({
                 status: true,
                 statusCode: 200,
                 message: "Surah retrieved data successfully",
                 data: Surah.data.data
             })
+            // if (id === '') {
+            //     const Surah = await instance.get(`${process.env.API_URL}`);
+            //     res.status(200).json({
+            //         status: true,
+            //         statusCode: 200,
+            //         message: "Surah retrieved data successfully",
+            //         data: Surah.data.data
+            //     })
+            // } else {
+            //     const Surah = await instance.get(`${process.env.API_URL}/${id}`);
+            //     res.status(200).json({
+            //         status: true,
+            //         statusCode: 200,
+            //         message: "Surah retrieved data successfully",
+            //         data: Surah.data.data
+            //     })
+            // }
         } catch (error) {
             console.error("Error while fetching data:", error);
             res.status(500).json({ message: "Internal Server Error" });
