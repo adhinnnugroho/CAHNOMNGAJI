6d87e85c95aaf9d3bc0725aaa4055092eca3ed2f
adhinnnugroho <adhinnnugroho@gmail.com>
2024-03-13 21:05:15 +0700

    simplefy api surah

--- src/pages/api/Surah/[[...surah]].ts
+++ src/pages/api/Surah/[[...surah]].ts
@@ -1,36 +1,27 @@
 import { retrieveData, retrieveDataByid } from "@/lib/RestApi/SurahApi/Service";
-import instance from "@/lib/axios/instance";
 import type { NextApiRequest, NextApiResponse } from "next";
-
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse,
+    res: NextApiResponse
 ) {
-    if (req.method === "GET") {
-        try {
-            const { surah }:any = req.query;
-            if(surah[1]) {
-                const id = surah[1];
-                const DetailsSurah = await retrieveDataByid(id);
-                res.status(200).json({
-                    status: true,
-                    statusCode: 200,
-                    message: "Surah retrieved data successfully",
-                    data: DetailsSurah.data.data
-                })
-            }else{
-                const Surah = await retrieveData();
-                res.status(200).json({
-                    status: true,
-                    statusCode: 200,
-                    message: "Surah retrieved data successfully",
-                    data: Surah.data.data
-                })
-            }
-        } catch (error) {
-            console.error("Error while fetching data:", error);
-            res.status(500).json({ message: "Internal Server Error" });
-        }
+    if (req.method !== "GET") {
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    try {
+        const { surah }: any = req.query;
+        const SurahId = surah ? surah[1] : null;
+        const data = SurahId ? await retrieveDataByid(SurahId) : await retrieveData();
+        
+        res.status(200).json({
+            status: true,
+            statusCode: 200,
+            message: "Surah retrieved data successfully",
+            data: data.data.data
+        });
+    } catch (error) {
+        console.error("Error while fetching data:", error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
